4b4f3ca456cb162f4d9af8d519546fb78ade214c
Igor-Gurskiy <iixrug@yandex.ru>
2025-07-08 15:34:54 +0300

    fix

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,16 +1,8 @@
-// import { useState } from 'react'
-
-
-
 import { Form } from '../form/Form'
 import { List } from '../list/List'
 import { Footer } from '../footer/Footer'
 import './App.scss'
 function App() {
-  
-
-
-
   return (
     <>
       <h1>todoes</h1>
